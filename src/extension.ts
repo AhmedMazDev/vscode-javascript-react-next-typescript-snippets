499f387819f913de97040ee7b0473e281e67cf52
Ahmed Maz Dev <ahmedmazdev@gmail.com>
2022-08-30 19:37:54 +0100

    Added ability to reload window on config change

--- src/extension.ts
+++ src/extension.ts
@@ -1,20 +1,31 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
 import * as vscode from "vscode";
 import generateSnippets from "./helpers/generateSnippets";
 import generatedSnippets from "./snippets/generatedSnippets.json";
 
-// this method is called when your extension is activated
-// your extension is activated the very first time the command is executed
+const showRestartMessage = async ({
+  affectsConfiguration,
+}: vscode.ConfigurationChangeEvent) => {
+  if (affectsConfiguration("reactSnippets")) {
+    await generateSnippets();
+    setTimeout(() => {
+      vscode.window
+        .showWarningMessage(
+          "React/NextJs Snippets: Please restart VS Code to apply snippet formatting changes",
+          "Restart VS Code",
+          "Ignore"
+        )
+        .then((action?: string) => {
+          if (action === "Restart VS Code") {
+            vscode.commands.executeCommand("workbench.action.reloadWindow");
+          }
+        });
+    }, 1000);
+  }
+};
+
 export async function activate(context: vscode.ExtensionContext) {
-  // Use the console to output diagnostic information (console.log) and errors (console.error)
-  // This line of code will only be executed once when your extension is activated
-  console.log(
-    'Congratulations, your extension "vscode-javascript-react-next-typescript-snippets" is now active!'
-  );
-
-  await generateSnippets();
+  vscode.workspace.onDidChangeConfiguration(showRestartMessage);
+  if (JSON.stringify(generatedSnippets).length < 10) {
+    await generateSnippets();
+  }
 }
-
-// this method is called when your extension is deactivated
-export function deactivate() {}
